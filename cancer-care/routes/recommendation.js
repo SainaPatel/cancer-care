73617b31d6cf893f2267afcd45f16aa76909af4c
donitaalmeida <donita.almeida@sjsu.edu>
2017-04-19 22:53:47 -0700

    Updated radonzone page to call backed API

--- cancer-care/routes/recommendation.js
+++ cancer-care/routes/recommendation.js
@@ -3,19 +3,21 @@ var mongoURL = "mongodb://cmpe295:cmpe295@ds161630.mlab.com:61630/radon";
 var json_responses={};
 
 exports.getRadonInfo=function(req,res){
-	var county="Alameda County";
+	var county="Alameda";
 	var state="California";
 	
 	mongo.connect(mongoURL,function(){
 		var coll=mongo.collection('regions');
-		coll.findOne({
-		    "county": county,
-		    "state":state
-		},function(err, region){
-			if (region) {
+				
+		
+		coll.find({
+		    "state":state,
+		    "county":county
+		}).toArray(function(err, regions){
+			if (regions) {
 				json_responses.status_code=200;
-				json_responses.data=region;
-				console.log(region);
+				json_responses.data=regions;
+				console.log(regions);
 				res.send( json_responses);
 
 			} else {
@@ -28,3 +30,22 @@ exports.getRadonInfo=function(req,res){
 	
 };
 
+exports.getRadonInfoGeneral=function(req,res){
+	mongo.connect(mongoURL,function(){
+		var coll=mongo.collection('states');
+				
+		coll.find({},{"code":1,"level":1}).toArray(function(err, states){
+			if (states) {
+				json_responses.status_code=200;
+				json_responses.data=states;
+				console.log(states);
+				res.send( json_responses);
+
+			} else {
+				json_responses.status_code=500;
+				console.log(err);
+				res.send(json_responses);
+			}
+		});
+	});
+};
