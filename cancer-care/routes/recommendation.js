23a22d373f6a027d639a8facd040ff5c1b77df0b
ritika04 <ritikashetty543@gmail.com>
2017-04-28 12:10:22 -0700

    Landing page updation

--- cancer-care/routes/recommendation.js
+++ cancer-care/routes/recommendation.js
@@ -1,6 +1,6 @@
 var mongo = require("./mongo");
 //var mongoURL = "mongodb://cmpe295:cmpe295@ds161630.mlab.com:61630/radon";
-var mongoURL = "mongodb://localhost:27017/test";
+var mongoURL = "mongodb://cmpe295lifestyle:cmpe295@ds111771.mlab.com:11771/circleofhope";
 var json_responses = {};
 
 exports.getRadonInfo = function (req, res) {
@@ -61,7 +61,7 @@ exports.getLifestyleArticles = function (req, res) {
 	});
 	console.log("Regex: " + rx);
 	mongo.connect(mongoURL, function () {
-		var coll = mongo.collection('articles');
+		var coll = mongo.collection('articlesLifestyle');
 		coll.find({
 			"Keywords": { $in: rx }
 		}).toArray(function (err, articles) {
