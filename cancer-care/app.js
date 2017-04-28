9cb123c4eb204af8ff4e3bffd3704d83fddfeabb
SainaPatel <saina.patel@sjsu.edu>
2017-04-27 19:32:07 -0700

    Alternative Medication Recommendation

--- cancer-care/app.js
+++ cancer-care/app.js
@@ -1,23 +1,24 @@
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  ,doctors=require('./routes/doctors')
-  , http = require('http')
-  , path = require('path')
-  , favicon = require('serve-favicon')
-  ,	recommendation=require('./routes/recommendation')
-  ,registration=require('./routes/registration')
-  ,profile = require('./routes/profile')
-  ,passport = require('passport')
-  ,session = require('express-session')
-  ,MongoStore=require('connect-mongo')(session)
-  ,LocalStrategy = require('passport-local').Strategy;
+, routes = require('./routes')
+, user = require('./routes/user')
+,doctors=require('./routes/doctors')
+,cam=require('./routes/cam')
+, http = require('http')
+, path = require('path')
+, favicon = require('serve-favicon')
+,	recommendation=require('./routes/recommendation')
+,registration=require('./routes/registration')
+,profile = require('./routes/profile')
+,passport = require('passport')
+,session = require('express-session')
+,MongoStore=require('connect-mongo')(session)
+,LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 
 
-// all environments
+//all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -30,11 +31,11 @@ app.use(favicon(path.join(__dirname,'public', 'favicon_cancer.ico')));
 
 
 app.use(session({  
-	  store:new MongoStore({url:'mongodb://cmpe295:cmpe295@ds023500.mlab.com:23500/session',collection:'sessions'}),
-	  secret: "best project ever",
-	  resave: false,
-	  saveUninitialized: false
-	}));
+	store:new MongoStore({url:'mongodb://cmpe295:cmpe295@ds023500.mlab.com:23500/session',collection:'sessions'}),
+	secret: "best project ever",
+	resave: false,
+	saveUninitialized: false
+}));
 app.use(passport.initialize());  
 app.use(passport.session());
 
@@ -42,41 +43,41 @@ app.use(app.router);
 
 //passport config
 passport.serializeUser(function (user, done) {
-    console.log("serializing " + user.email);
-    done(null, user.email);
+	console.log("serializing " + user.email);
+	done(null, user.email);
 });
 
 passport.deserializeUser(function (email, done) {
-	
-    console.log("deserializing " + email);
-    registration.findUserById(email, done);
 
-    //done(null, user);
+	console.log("deserializing " + email);
+	registration.findUserById(email, done);
+
+	//done(null, user);
 });
 
 passport.use( new LocalStrategy({ passReqToCallback : true,usernameField: 'email'},
-	    function (req,email, password, done) {
-			console.log(email+" | "+password);
-	        registration.findUser(email,password, function (err, user) {
-	            if (err) {
-	                return done(err);
-	            }
-	            if (!user) {
-	                return done(null, false);
-	            }
-	            	 return done(null, user);     
-	        });
-	    }
-	));
+		function (req,email, password, done) {
+	console.log(email+" | "+password);
+	registration.findUser(email,password, function (err, user) {
+		if (err) {
+			return done(err);
+		}
+		if (!user) {
+			return done(null, false);
+		}
+		return done(null, user);     
+	});
+}
+));
 
 //use later
 passport.authenticationMiddleware = function() {
-    return function (req, res, next) {
-        if (req.isAuthenticated()) {
-            return next();
-        }
-        res.redirect('/');
-    };
+	return function (req, res, next) {
+		if (req.isAuthenticated()) {
+			return next();
+		}
+		res.redirect('/');
+	};
 };
 
 /*function loggedIn(req, res, next) {
@@ -94,10 +95,10 @@ passport.authenticationMiddleware = function() {
     }
 
 }
-*/
+ */
 //development only
 if ('development' == app.get('env')) {
-app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 app.get('/', routes.index);
 app.get('/users', user.list);
@@ -105,34 +106,34 @@ app.get('/radoninfo',recommendation.getRadonInfo);
 app.get('/getLifestyleArticles', passport.authenticationMiddleware(),recommendation.getLifestyleArticles);
 app.get('/radoninfoGeneral',passport.authenticationMiddleware(),recommendation.getRadonInfoGeneral);
 app.get('/successLogin',function(req,res){
-	
+
 	res.status(200).send("success");
 });
 app.get('/faliureLogin',function(req,res){
 	res.status(401).send("Unauthorized");
 });
 app.get('/patientProfile',passport.authenticationMiddleware(),function(req,res){
-	 res.render('patientProfile', { title: 'Circle of Hope' });
+	res.render('patientProfile', { title: 'Circle of Hope' });
 });
 app.get('/doctorProfile',passport.authenticationMiddleware(),function(req,res){
-	 res.render('doctorProfile', { title: 'Circle of Hope' });
+	res.render('doctorProfile', { title: 'Circle of Hope' });
 });
 app.post('/register',registration.patientRegister);
 //app.post('/login',registration.login);
 app.post('/login', passport.authenticate('local', {
-    successRedirect: '/successLogin',
-    failureRedirect: '/faliureLogin'
+	successRedirect: '/successLogin',
+	failureRedirect: '/faliureLogin'
 }));
 app.get('/logout', function(req, res){
-	  var name = req.user.email;
-	  console.log("LOGGIN OUT " + name);
-	  req.logout();
-	  res.redirect('/');
-	});
+	var name = req.user.email;
+	console.log("LOGGIN OUT " + name);
+	req.logout();
+	res.redirect('/');
+});
 app.post('/addFACTL',profile.addFACTL);
 app.post('/doctorRegister',registration.doctorRegister);
 app.post('/getDoctors',doctors.getDoctors);
-
+app.get('/getCamTreatments',cam.getCamTreatments);
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+	console.log('Express server listening on port ' + app.get('port'));
 });
